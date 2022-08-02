8440ef0ec7a2f2ad796b8aa5dba2f2a4a729aa77
BiagioCozzolino <biagiocozzolino1996@gmail.com>
2022-08-02 15:12:08 +0200

    Eseguita function per calcolo prezzo del biglietto e verificato con console log

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+const scontoMinori = 0.2;
+const scontoOver65 = 0.4;
+const prezzoPerKm = 0.21;
+let prezzoInziale=0;
+
+function calcoloPrezzoBiglietto(kilometri,eta){
+    prezzoInziale= kilometri * prezzoPerKm
+    if (eta > 65) {
+        return prezzoInziale - (prezzoInziale * scontoOver65);
+    } else if (eta < 18) {
+        return prezzoInziale - (prezzoInziale * scontoMinori);
+    } else {
+        return prezzoInziale;
+    }
+}
+
+console.log(calcoloPrezzoBiglietto(21,5))
+console.log(calcoloPrezzoBiglietto(21,67))
+console.log(calcoloPrezzoBiglietto(21,22))
